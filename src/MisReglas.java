3c58a5387f78dfab07fbb31ddb4a0760d3b68417
IN1DM3B_15 <IN1DM3B_15@H015112.NET>
2019-12-10 09:50:08 +0100

    Regla 3

--- src/MisReglas.java
+++ src/MisReglas.java
@@ -4,9 +4,10 @@ public class MisReglas {
 	private Regla[] misReglas;
 	
 	public MisReglas() {
-		misReglas = new Regla[2];
+		misReglas = new Regla[3];
 		misReglas[0] = new Regla1();
 		misReglas[1] = new Regla2();
+		misReglas[2] = new Regla3();
 	}
 
 	public Regla[] getMisReglas() {
---
+++ src/Regla3.java
@@ -0,0 +1,26 @@
+
+public class Regla3 extends Regla{
+	
+	public Regla3() {
+		setRuleName();
+	}
+
+	@Override
+	public boolean patronear(String entrada) {
+		
+		String primeraLetra = entrada.substring(0,1);
+		String primeraLetraMayuscula = entrada.substring(0,1).toUpperCase();
+		
+		if (primeraLetra.equals(primeraLetraMayuscula)) {
+			return true;
+	 	}else {
+	 		return false;
+	 	}	
+	
+	}
+
+	public void setRuleName() {
+		this.name = "si3";
+	}
+	
+}
